0bce507855a12c511d299201df56fa00d0f459c4
bitwang <chuenlungwang@gmail.com>
2015-11-02 08:25:19 +0800

    日志服务调用函数注释

--- skynet-src/skynet_error.c
+++ skynet-src/skynet_error.c
@@ -10,6 +10,9 @@
 
 #define LOG_MESSAGE_SIZE 256
 
+/* 向名字为 logger 的日志服务发送日志消息, 此函数中 msg 为格式化字符串,
+ * 后边的不定参数是其替换对象. 函数要求调用自行管理格式字符串和替换对象的内存.
+ * 函数保证能够完整格式化消息, 但是不保证消息一定能够输出, 因为有可能日志服务已经被销毁了. */
 void 
 skynet_error(struct skynet_context * context, const char *msg, ...) {
 	static uint32_t logger = 0;
@@ -21,16 +24,19 @@ skynet_error(struct skynet_context * context, const char *msg, ...) {
 	}
 
 	char tmp[LOG_MESSAGE_SIZE];
-	char *data = NULL;
+	char *data = NULL;           /* data 为发送的数据, 必须指向堆内存 */
 
 	va_list ap;
 
+	/* 将消息格式化写入 tmp 数组中, 仅当 len 小于 数组长度时表示全部写入 */
 	va_start(ap,msg);
+	/* [fix]有可能有编码错误, 返回值 len 将为负数. [/fix] */
 	int len = vsnprintf(tmp, LOG_MESSAGE_SIZE, msg, ap);
 	va_end(ap);
 	if (len < LOG_MESSAGE_SIZE) {
 		data = skynet_strdup(tmp);
 	} else {
+		/* 循环直到能够完全写入, 注意堆内存管理 */
 		int max_size = LOG_MESSAGE_SIZE;
 		for (;;) {
 			max_size *= 2;
@@ -55,6 +61,7 @@ skynet_error(struct skynet_context * context, const char *msg, ...) {
 	smsg.session = 0;
 	smsg.data = data;
 	smsg.sz = len | ((size_t)PTYPE_TEXT << MESSAGE_TYPE_SHIFT);
+	/* [fix]应该检查错误, 并回收 data 的内存. [/fix] */
 	skynet_context_push(logger, &smsg);
 }
 
