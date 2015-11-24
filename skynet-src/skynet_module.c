6e5074f7a4a720da768842756d6b236799b7c880
bitwang <chuenlungwang@gmail.com>
2015-11-24 23:21:13 +0800

    skynet module comment

--- skynet-src/skynet_module.c
+++ skynet-src/skynet_module.c
@@ -10,17 +10,23 @@
 #include <stdint.h>
 #include <stdio.h>
 
+/* 最多只能容纳 MAX_MODULE_TYPE 个服务模块 */
 #define MAX_MODULE_TYPE 32
 
+/* 所有服务模块的收集器结构 */
 struct modules {
-	int count;
-	struct spinlock lock;
-	const char * path;
-	struct skynet_module m[MAX_MODULE_TYPE];
+	int count;                                  /* 当前已经启动的模块数 */
+	struct spinlock lock;                       /* 并发操作收集器时需要加锁 */
+	const char * path;                          /* 模块 so 文件所在目录模式 */
+	struct skynet_module m[MAX_MODULE_TYPE];    /* 保存服务模块结构的数组 */
 };
 
 static struct modules * M = NULL;
 
+/* 尝试打开名字为 name 的服务模块, 并将其保存于管理器 m 中. 目录模式 path 用分号分割, 并用 name
+ * 替换其中每一个问号依次查询服务模块, 直到找到或者最终无法找到为止.
+ * 参数: m 是模块管理器, name 是服务模块的名字.
+ * 返回: 服务模块的指针, 如果不存此服务模块返回 NULL . */
 static void *
 _try_open(struct modules *m, const char * name) {
 	const char *l;
@@ -34,6 +40,7 @@ _try_open(struct modules *m, const char * name) {
 	char tmp[sz];
 	do
 	{
+		/* 以分号为分隔符, 以问号为替换符, 得到所有的服务模块 so 文件地址. */
 		memset(tmp,0,sz);
 		while (*path == ';') path++;
 		if (*path == '\0') break;
@@ -62,6 +69,9 @@ _try_open(struct modules *m, const char * name) {
 	return dl;
 }
 
+/* 在管理器重查询名字为 name 的服务模块. 如果存在此模块就返回其模块指针, 否则返回 NULL .
+ * 参数: name 为模块的名字
+ * 返回: 模块的地址. */
 static struct skynet_module * 
 _query(const char * name) {
 	int i;
@@ -73,6 +83,10 @@ _query(const char * name) {
 	return NULL;
 }
 
+/* 查询服务模块 mod 中的 4 个生命周期函数, 并保存在相应的模块字段中. 如果相应的函数不存在, 其字段将设置为
+ * NULL. 函数返回 init 函数是否为 NULL .
+ * 参数: mod 是服务模块.
+ * 返回: 0 表示服务模块的 init 函数不为 NULL, 1 表示 init 函数为 NULL. */
 static int
 _open_sym(struct skynet_module *mod) {
 	size_t name_size = strlen(mod->name);
@@ -90,6 +104,9 @@ _open_sym(struct skynet_module *mod) {
 	return mod->init == NULL;
 }
 
+/* 查询名字 name 所代表的服务模块并返回服务模块指针. 如果服务模块还没有加载,
+ * 就将其加载到管理器中再返回服务模块指针.
+ * 参数: name 是服务模块的名字. */
 struct skynet_module * 
 skynet_module_query(const char * name) {
 	struct skynet_module * result = _query(name);
@@ -120,6 +137,9 @@ skynet_module_query(const char * name) {
 	return result;
 }
 
+/* 向管理器中插入服务模块 mod , 要求此服务模块之前不存在与管理器中.
+ * 参数: mod 是服务模块
+ * 此函数没有返回值. */
 void 
 skynet_module_insert(struct skynet_module *mod) {
 	SPIN_LOCK(M)
@@ -133,6 +153,10 @@ skynet_module_insert(struct skynet_module *mod) {
 	SPIN_UNLOCK(M)
 }
 
+/* 创建一个模块 m 的服务实例. 如果模块无法创建实例, 将返回最大无符号整形作为服务地址.
+ * 服务模块最终会在初始化和释放服务时对此地址进行合理解释和利用.
+ * 参数: m 为服务模块
+ * 返回: 服务实例的地址 */
 void * 
 skynet_module_instance_create(struct skynet_module *m) {
 	if (m->create) {
@@ -142,11 +166,17 @@ skynet_module_instance_create(struct skynet_module *m) {
 	}
 }
 
+/* 初始化服务实例. 传递给服务实例所属的服务对象 ctx 以及相关的初始化参数, 每个服务模块都必须有初始化方法.
+ * 参数: m 是服务实例所属的服务模块, inst 是服务实例, ctx 是服务对象, parm 是初始化参数.
+ * 返回: 初始化成功时返回 0 , 失败时返回 1 . */
 int
 skynet_module_instance_init(struct skynet_module *m, void * inst, struct skynet_context *ctx, const char * parm) {
 	return m->init(inst, ctx, parm);
 }
 
+/* 释放服务实例 inst , 如果服务模块没有释放函数将不做任何操作.
+ * 参数: m 是服务实例所属的服务模块, inst 是服务实例.
+ * 此函数没有返回值. */
 void 
 skynet_module_instance_release(struct skynet_module *m, void *inst) {
 	if (m->release) {
@@ -154,7 +184,7 @@ skynet_module_instance_release(struct skynet_module *m, void *inst) {
 	}
 }
 
-/* 向服务实例 inst 发送信号.
+/* 向服务实例 inst 发送信号, 如果服务没有发送信号的函数将不做任何操作.
  * 参数: m 是服务实例所属的服务模块, inst 是服务实例, signal 是信号.
  * 此函数没有返回值. */
 void
